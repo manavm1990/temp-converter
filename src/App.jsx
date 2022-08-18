a3bd9a6d82c12602d0eca8d1756707d5c1825460
Manav Misra <manavm@visionwebsoft.com>
2022-08-18 08:10:33 -0500

    Render 2 dumb Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
-// TODO: Import and render 2 of the inputs
+import Input from "./components/Input/Input";
 
 function App() {
   return (
-    <h1 className="my-4 text-center text-2xl font-bold">
-      Temperature Converter
-    </h1>
+    <>
+      <h1 className="my-4 text-center text-2xl font-bold">
+        Temperature Converter
+      </h1>
+      <Input />
+      <Input />
+    </>
   );
 }
 
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input() {
+  return (
+    <div>
+      <label htmlFor="fahrenheit">Fahrenheit</label>
+      <input type="number" id="fahrenheit" />
+    </div>
+  );
+}
