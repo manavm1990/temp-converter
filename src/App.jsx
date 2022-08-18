67c6c7f33e03c99f6bb681ed3499718f1c1f6ae8
Manav Misra <manavm@visionwebsoft.com>
2022-08-18 08:07:01 -0500

    Add h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 // TODO: Import and render 2 of the inputs
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="my-4 text-center text-2xl font-bold">
+      Temperature Converter
+    </h1>
+  );
 }
 
 export default App;
