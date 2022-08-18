954b4ccb42e569fa20878d4121f651c426e8cd74
Manav Misra <manavm@visionwebsoft.com>
2022-08-18 11:13:41 -0500

    Replace hard-coded values with state values

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
 import "./App.css";
 import Input from "./components/Input/Input";
+import tryConvert from "./lib/tryConvert";
 
 function App() {
+  const [values] = React.useState({ celsius: 0, fahrenheit: 0 });
+
   const handleChange = (event) => {
     console.log(event.target.value);
+
+    if (event.target.id === "celsius") {
+      console.log(tryConvert(event.target.value, "toFahrenheit"));
+    } else {
+      console.log(tryConvert(event.target.value, "toCelsius"));
+    }
   };
 
   return (
@@ -13,8 +23,16 @@ function App() {
       </h1>
 
       <form className="mx-auto mt-8 flex w-1/3 justify-between">
-        <Input label="Celsius" value={100} changeHandler={handleChange} />
-        <Input label="Fahrenheit" value={50} changeHandler={handleChange} />
+        <Input
+          label="Celsius"
+          value={values.celsius}
+          changeHandler={handleChange}
+        />
+        <Input
+          label="Fahrenheit"
+          value={values.fahrenheit}
+          changeHandler={handleChange}
+        />
       </form>
     </>
   );
