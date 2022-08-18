201453785011ae602dd30e09cddde3a8f704220e
Manav Misra <manavm@visionwebsoft.com>
2022-08-18 08:44:23 -0500

    Make labels dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ function App() {
       <h1 className="my-4 text-center text-2xl font-bold">
         Temperature Converter
       </h1>
-      <Input />
-      <Input />
+
+      <form className="mx-auto mt-8 flex w-1/3 justify-between">
+        <Input label="Celsius" />
+        <Input label="Fahrenheit" />
+      </form>
     </>
   );
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,18 @@
-export default function Input() {
+import PropTypes from "prop-types";
+
+export default function Input({ label }) {
   return (
-    <div>
-      <label htmlFor="fahrenheit">Fahrenheit</label>
-      <input type="number" id="fahrenheit" />
+    <div className="flex flex-col gap-y-1">
+      <label htmlFor={label.toLowerCase()}>{label}</label>
+      <input
+        type="number"
+        id={label.toLowerCase()}
+        className="w-36 rounded border"
+      />
     </div>
   );
 }
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+};
