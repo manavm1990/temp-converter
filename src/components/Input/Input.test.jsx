12e5661eaccbc643283664a2abec33bd8cb5b258
Manav Misra <manavm@visionwebsoft.com>
2022-08-19 07:49:37 -0500

    Refactor tests to use variables
    Makes teseting a bit more dynamic and less rigid.

--- src/components/Input/Input.test.jsx
+++ src/components/Input/Input.test.jsx
@@ -12,19 +12,22 @@ it("renders correctly", () => {
 });
 
 it("calls changeHandler whenever input is changed", () => {
+  const label = "celsius";
+  const txtInput = "1000";
+
   // Arrange - create a mock for the changeHandler function
   const changeHandler = jest.fn();
 
   // Act - render the component
   // Avoid using an empty string for the label
-  render(<Input label="celsius" value={0} changeHandler={changeHandler} />);
+  render(<Input label={label} value={0} changeHandler={changeHandler} />);
 
   // As per accessibility standards, the input element is labelled with the label prop
-  const input = screen.getByLabelText("celsius");
+  const input = screen.getByLabelText(label);
 
   // Each key that is typed into the input should result in the changeHandler mock being called
-  userEvent.type(input, "100");
+  userEvent.type(input, txtInput);
 
   // Assert - check that the changeHandler was called
-  expect(changeHandler).toHaveBeenCalledTimes(3);
+  expect(changeHandler).toHaveBeenCalledTimes(txtInput.length);
 });
